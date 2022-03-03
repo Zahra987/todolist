da95add66d1249ff6d35457f68df3c090c3da033
zahra987 <zhr.ramezanpour@gmail.com>
2022-03-03 11:33:03 +0330

    second2

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -6,31 +6,34 @@ export default function Main() {
     const [inputValue , setinputValue] = useState("");
     const [listState , setListState] = useState([]);
     const useNameRef = useRef();
- const toDoList = listState.map((todo,index)=>(
-   <List key={index} work={todo} />
- ))
- function handleClick(){
-   if( !inputValue == "")
-   { setListState( listState => [...listState , inputValue  ]);
-  setinputValue("");}
-    else window.alert("please type something");
-  }
-useEffect(()=>{
-  useNameRef.current.focus();
-} );
+
+
+    const toDoList = listState.map((todo,index)=>(
+   <List key={index} work={todo} />))
+
+
+    function handleClick(){
+      if( !inputValue == "")
+          {setListState( listState => [...listState , inputValue  ]);
+          setinputValue("");}
+      else window.alert("please type something");}
+
+
+    useEffect(()=>{
+          useNameRef.current.focus();
+              });
+
+
   return (
     <>
     <div id="myDIV" className="header">
         <h2 >My To Do List</h2>
         <input ref={useNameRef}  type="text" id="myInput" placeholder="Title..." value={inputValue}
-         onChange={(e)=>{setinputValue(e.target.value)}}  />
+         onChange={(e)=>{setinputValue(e.target.value);}} />
             <span onClick={handleClick}
                   className="addBtn">Add
             </span>
     </div>
   {toDoList}
-        
-   
-</>
-  )
-}
+    </>
+  )}
